80ec4c3eef9b8e2cbe9872abd4e52388ebfcaec9
tyler rinker <tyler.rinker@gmail.com>
2014-01-10 11:23:28 -0500

    added examples R script for addng gridlines

---
+++ movie2frames/add_gridlines.R
@@ -0,0 +1,57 @@
+## add gridlines
+## http://stackoverflow.com/a/21043587/1000343
+## Compliments of Troy
+
+# pre-req libraries
+p_load(ggplot2, grid, png)      # to read the PNG file
+
+grid_calc <- function(width = 10, height = width) {
+    # generate the points and labels for the grid
+    points <- data.frame(expand.grid(w=1:width, h=1:height))
+    points$labs <-paste0("(", points$w, "," ,points$h, ")")
+    points$x <- points$w-0.5 # center
+    points$y <- points$h-0.5
+    
+    # make the gridline co-ordinates
+    gridx <- data.frame(x=0:width, xend=0:width, y=rep(0,width+1), 
+    	yend=rep(height, width+1))
+    gridy <- data.frame(x=rep(0, height+1), xend=rep(width, height+1), 
+    	y=0:height, yend=0:height)
+    list(coords = rbind(gridx, gridy), w = width, h = height, points = points)
+}
+
+grid_dat <- grid_calc(20)
+# function to plot using ggplot with annotation_custom for the image
+plotgrid <- function(file, coord.color = "gray60", grid.color = coord.color, 
+	text.size = 3, grid.data = grid_dat){
+	width <- grid.data[["w"]]
+	height <- grid.data[["h"]]
+    ggplot(grid.data[["points"]])+theme_bw()+
+        annotation_custom(rasterGrob(readPNG(file), 0, 0, 1, 1, 
+        	just=c("left", "bottom")), 0, width, 0, height)+
+        geom_text(aes(x=x, y=y, label=labs), color=coord.color, 
+        	size = text.size)+
+        geom_segment(aes(x=x, xend=xend, y=y, yend=yend), 
+        	data=grid.data[["coords"]], color=grid.color) +
+        coord_cartesian(c(0, width), c(0, height))
+}
+
+# run the function for each file in the folder
+wd <- getwd()
+setwd("movie2frames/out/out_05")
+xpath <- folder(gridout)
+## xpath <- folder(gridout_20)
+fls <- dir()[-c(1:2)]
+#t1 <- Sys.time()
+w <- 800
+h <- w
+lapply(fls,function(x){
+	png(file.path(xpath, x), width=w, height=h)
+	print(plotgrid(x))
+	dev.off()
+})
+#t2 <- Sys.time()
+#difftime(t2, t1) #33.40391 on 38f iles
+setwd(wd)
+
+
--- movie2frames/frames_script.R
+++ movie2frames/frames_script.R
@@ -1,20 +1,30 @@
 ## http://stackoverflow.com/questions/3917601/ffmpeg-split-avi-into-frames-with-known-frame-rate
 ## http://bioeng-student.blogspot.com/2011/05/splitting-video-into-frames-using.html
+## http://linuxaria.com/howto/tricks-ffmpeg?lang=en
 
-#getwd()
+wd <- getwd()
 setwd("movie2frames")
-browseURL("https://copy.com/web/spliced.mp4?download=1")
+x <- folder(out)
+setwd(x)
+fls <-file.path("movie2frames/out", rdirs(out, 1:10, text.only = TRUE))
+folder(folder.name = fls)
+
+
+##  browseURL("https://copy.com/web/spliced.mp4?download=1")
 ##  move to working directory
 
 ## cd GitHub/density/movie2frames  ## shell command
 
 ## Splice
-ffmpeg -i truly_amazing_teacher.mp4 -ss 00:00:20.0 -c copy -t 00:00:35.0 spliced.mp4
+## ffmpeg -i truly_amazing_teacher.mp4 -ss 00:00:20.0 -c copy -t 00:00:35.0 spliced.mp4
+
+shell("ffmpeg -i spliced.mp4 -r 1 -s 500x500 -f image2 out/out_05/image-%07d.png")
 
 ## movie to frames
-ffmpeg -i spliced.mp4 -f image2 out1/image-%07d.png
-ffmpeg -i spliced.mp4 -r 1000 -f image2 out2/image-%07d.png
-ffmpeg -i spliced.mp4 -r 1 -f image2 out3/image-%07d.png
-ffmpeg -i spliced.mp4 -r 5 -f image2 out4/image-%07d.png
+ffmpeg -i spliced.mp4 -f image2 out/out_01/image-%07d.png
+ffmpeg -i spliced.mp4 -r 1000 -f out/image2 out_02/image-%07d.png
+ffmpeg -i spliced.mp4 -r 1 -f image2 out/out_03/image-%07d.png
+ffmpeg -i spliced.mp4 -r 5 -f image2 out/out_04/image-%07d.png
 
 
+setwd(wd)
